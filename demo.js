434e501b534bb68446a61b2b6452bb045e893088
bguiz <bguiz@users.noreply.github.com>
2020-12-22 13:46:36 +0800

    test: update test to use new structure

--- demo.js
+++ demo.js
@@ -1,11 +1,12 @@
-const ipfs = require('./rif-storage-util.js').default.ipfs;
+const ipfsUtil = require('./rif-web-util.js')
+  .default.rifStorageUtil.ipfsUtil;
 
 const dirPath = process.argv[2] || './test-data/';
-ipfs.readDirContents(dirPath)
-  .then(ipfs.uploadToIpfs)
-  .then(ipfs.printContents) // to demo that previous step worked
-  .then(ipfs.pinToIpfs)
-  .then(ipfs.printContents) // to demo that previous step worked
+ipfsUtil.readDirContents(dirPath)
+  .then(ipfsUtil.uploadToIpfs)
+  .then(ipfsUtil.printContents) // to demo that previous step worked
+  .then(ipfsUtil.pinToIpfs)
+  .then(ipfsUtil.printContents) // to demo that previous step worked
   .catch((err) => {
     console.error(err);
     process.exit(1);
